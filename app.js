1a24dbbc9a578073aa53c6d278163cd920a83c1b
Praveen-singh-kushwah <98682189+Praveen-singh-kushwah@users.noreply.github.com>
2024-02-06 12:06:46 +0530

    thirdl commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Name:- Praveen ; En no. :- 21162101021 ; SUB :- EADC');
-  });
-  const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`API server listening on port ${port}`);
 });
